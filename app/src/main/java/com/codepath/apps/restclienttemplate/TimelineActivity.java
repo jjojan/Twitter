b2f59691fbd64079e5b7b47f287a1632a7fab4c8
Jefrin Jojan <jefrin@fb.com>
2022-06-07 17:32:51 -0700

    Setup on TwitterApp/Client for showing tweets in Timeline and allows composing of tweets

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -0,0 +1,127 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.ComposeActivity;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class TimelineActivity extends AppCompatActivity {
+
+
+    public static final String TAG = "TIMEACT";
+    RecyclerView rvTweets;
+    TwitterClient client;
+    List<Tweet> tweets;
+    TweetsAdapter adapter;
+    ActivityResultLauncher<Intent> messActLauncher;
+    private final int REQUEST_CODE = 20;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        Button logoutButton = findViewById(R.id.logoutButton);
+
+        client = TwitterApplication.getRestClient(this);
+        rvTweets = findViewById(R.id.rvTweets);
+
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(this, tweets);
+
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
+
+        populateHomeTimeline();
+
+
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLogoutButton();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.Compose){
+            Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                JSONArray jsonArray = json.jsonArray;
+
+                try {
+                    tweets.addAll(Tweet.fromJSONArray(jsonArray));
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSON", e);
+                }
+
+                Log.i(TAG, "sucess");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.i(TAG, "onFail", throwable);
+            }
+        });
+    }
+
+    void onLogoutButton() {
+        // forget who's logged in
+        TwitterApplication.getRestClient(this).clearAccessToken();
+
+        // navigate backwards to Login screen
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+        //startActivityForResult(i, REQUEST_CODE);
+
+    }
+
+
+
+
+
+
+}
+
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/ComposeActivity.java
@@ -0,0 +1,71 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.TwitterApplication;
+import com.codepath.apps.restclienttemplate.TwitterClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+
+import okhttp3.Headers;
+
+public class ComposeActivity extends AppCompatActivity {
+
+    EditText etCompose;
+    Button btnTweet;
+    public static final String TAG = "ComposeActivity";
+    public static final int MAX_CHAR_COUNT  = 280;
+    TwitterClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+        client = TwitterApplication.getRestClient(this );
+
+        etCompose = findViewById(R.id.etCompose);
+        btnTweet =  findViewById(R.id.btnTweet);
+
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tweetContent = etCompose.getText().toString();
+                if(tweetContent.isEmpty()){
+                    Toast.makeText(ComposeActivity.this, "Sorry your tweet is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(tweetContent.length() > MAX_CHAR_COUNT){
+                    Toast.makeText(ComposeActivity.this, "Sorry your tweet is too long", Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(ComposeActivity.this,tweetContent, Toast.LENGTH_LONG).show();
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "onSucess to publish tweet");
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG, "Published Tweet" +tweetContent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                    Log.e(TAG, "onFailure to publish tweet", throwable);
+                    }
+                });
+            }
+        });
+    }
+}
